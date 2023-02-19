d2a8127531bf47f7850bcf0012a828336a45b5c6
DedeMarantes <andre.marantes@gmail.com>
2023-02-18 23:05:38 -0300

    Adicionado migracoes e soft delete e alterando controlador

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -62,6 +62,18 @@ class PessoaController {
         }
     }
 
+    static async restauraPessoa(req, res) {
+        const {id} = req.params
+        try {
+            await database.Pessoas.restore({ where: {
+                id: Number(id)
+            }})
+            return res.status(200).json({mensagem: `id ${id} é restaurado`})
+        } catch (error) {
+            return res.send(500).json(error.message)
+        }
+    }
+
     static async getMatricula(req, res) {
         const { estudanteId, matriculaId } = req.params;
         try {
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -6,12 +6,13 @@ const router = Router();
 router
 .get('/pessoas', PessoaController.getAllPessoas)
 .get('/pessoas/:id', PessoaController.getPessoa)
-.post('/pessoas', PessoaController.criarPessoa)
-.put('/pessoas/:id', PessoaController.atualizarPessoa)
-.delete('/pessoas/:id', PessoaController.deletarPessoa)
 .get('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.getMatricula)
+.post('/pessoas', PessoaController.criarPessoa)
 .post('/pessoas/:estudanteId/matriculas', PessoaController.criaMatricula)
+.post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
+.put('/pessoas/:id', PessoaController.atualizarPessoa)
 .put('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.atualizaMatricula)
+.delete('/pessoas/:id', PessoaController.deletarPessoa)
 .delete('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.deletarMatricula)
 
 module.exports = router;
